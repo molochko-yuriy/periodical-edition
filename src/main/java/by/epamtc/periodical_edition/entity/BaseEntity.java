932aec7aba6adcc62dee78713316b129701470e3
Molochko <molochko.uriy@gmail.com>
2021-11-11 01:27:40 +0300

    creating BaseEntity

---
+++ src/main/java/by/epamtc/periodical_edition/entity/BaseEntity.java
@@ -0,0 +1,13 @@
+package by.epamtc.periodical_edition.entity;
+
+public abstract class BaseEntity <T> {
+    private T id;
+
+    public T getId() {
+        return id;
+    }
+
+    public void setId(T id) {
+        this.id = id;
+    }
+}
