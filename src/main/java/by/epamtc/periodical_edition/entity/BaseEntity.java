d4a2d484e16ad5e7b741ca28ee0131cf578d8d78
Molochko <molochko.uriy@gmail.com>
2021-11-16 18:29:08 +0300

    changing base entity

--- src/main/java/by/epamtc/periodical_edition/entity/BaseEntity.java
+++ src/main/java/by/epamtc/periodical_edition/entity/BaseEntity.java
@@ -1,13 +1,13 @@
 package by.epamtc.periodical_edition.entity;
 
-public abstract class BaseEntity <T> {
-    private T id;
+public abstract class BaseEntity <E> {
+    private E id;
 
-    public T getId() {
+    public E getId() {
         return id;
     }
 
-    public void setId(T id) {
+    public void setId(E id) {
         this.id = id;
     }
 }
